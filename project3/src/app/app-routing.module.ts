c8b16bf3041117010379ee177ad2a4f099555162
joseph lamacchia <joseph.lamacchia@revature.net>
2021-08-17 16:54:16 -0400

    editing set manager

--- project3/src/app/app-routing.module.ts
+++ project3/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SetManagerComponent } from './set-manager/set-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'setmanager', component: SetManagerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project3/src/app/set-manager/set-manager.component.ts
+++ project3/src/app/set-manager/set-manager.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-manager.component.css']
 })
 export class SetManagerComponent implements OnInit {
+ setName = '';
+ selected:any;
+ data:Array<Object> = [
+  {id: 0, name: "name1"},
+  {id: 1, name: "name2"}
+];
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+showSelected(){
+
+  //this will poulate table based on the set name
+  console.log(this.selected);
+}
 
 }
