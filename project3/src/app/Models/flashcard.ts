1398be71950470a2a64d9a8a5998cdaf0bc8516b
Samantha Sanchez <sammy@MacBook-Pro.anselm.edu>
2021-08-17 16:04:55 -0400

    user,flashcard and flashcard set models

---
+++ project3/src/app/Models/flashcard.ts
@@ -0,0 +1,9 @@
+  export class flashcard {
+    question: string;
+    answer: string;
+  
+    constructor(question: string, answer: string){
+      this.question = question;
+      this.answer = answer;
+    }
+  }
