37bd467978c3dccea1ab8f6a788e38e783c5427c
SeanGRev <sean.guo@revature.net>
2021-08-18 12:15:16 -0400

    Added Previous card and Next card swapping

--- project3/src/app/Components/cardrunner/cardrunner.component.ts
+++ project3/src/app/Components/cardrunner/cardrunner.component.ts
@@ -22,9 +22,16 @@ export class CardrunnerComponent implements OnInit {
 
   public progress:number = 85;
   public progress2:string = this.progress + "%";
+  public crnt = 0;
 
-  public question:string = 'QUESTION';
-  public answer:string = 'ANSWER';
+  testCards: Array<any> = [
+    {'question': 'question1', 'answer': 'answer1'},
+    {'question': 'question2', 'answer': 'answer2'},
+    {'question': 'question3', 'answer': 'answer3'}
+  ]
+
+  public question:string = this.testCards[this.crnt].question;
+  public answer:string = this.testCards[this.crnt].answer;
 
 
   constructor() { }
@@ -36,6 +43,29 @@ export class CardrunnerComponent implements OnInit {
 
   toggleFlip() {
     this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+  }
+
+  nextQuestion(){
+    var count = Object.keys(this.testCards).length;
+
+    if(this.crnt != (count - 1)){
+      if(this.flip == 'active'){
+        this.flip = 'inactive';
+      }
+      this.question = this.testCards[this.crnt + 1].question;
+      this.answer = this.testCards[this.crnt + 1].answer;
+      this.crnt = this.crnt + 1;
+    }
+
+  }
+
+  prevQuestion(){
+    
+    if(this.crnt != 0){
+      this.question = this.testCards[this.crnt - 1].question;
+      this.answer = this.testCards[this.crnt - 1].answer;
+      this.crnt = this.crnt - 1;
+    }
   }
 
 }
