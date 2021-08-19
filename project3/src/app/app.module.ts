924c3e9d17af2296abcad6f50063a849c26bd7c7
joseph lamacchia <joseph.lamacchia@revature.net>
2021-08-19 13:48:42 -0400

    adding rating functionality as component.

--- project3/src/app/app.module.ts
+++ project3/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+//Components
 import { AppComponent } from './app.component';
 import { SetManagerComponent } from './Components/set-manager/set-manager.component';
-import { FormsModule } from '@angular/forms';
 import { CardrunnerComponent } from './Components/cardrunner/cardrunner.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
+import { StarsComponent } from './Components/stars/stars.component';
 
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     SetManagerComponent,
     CardrunnerComponent,
-    HomepageComponent
+    HomepageComponent,
+    StarsComponent
     
   ],
   imports: [
@@ -23,6 +28,7 @@ import { HomepageComponent } from './homepage/homepage.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule
     
 
   ],
